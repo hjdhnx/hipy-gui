a14c306fa6c136d0b759806f86a1485ebb2ada19
晚风拂柳颜 <434857005@qq.com>
2024-03-22 18:55:42 +0800

    初始化项目

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+import os
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
+from pathlib import Path
+
+app = FastAPI()
+
+ROOT_PATH = os.path.join(os.path.dirname(__file__), "..")
+public_file_abspath = os.path.join(ROOT_PATH, "public")
+public_file_abspath = Path(public_file_abspath).as_posix()
+os.makedirs(public_file_abspath, exist_ok=True)
+app.mount("/", StaticFiles(directory=public_file_abspath), name="public")
+
+
+@app.get("/api")
+def index():
+    return JSONResponse({"status": 200, "msg": "ok"})
